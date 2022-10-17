2197bc3db6970a6a98d660807e5d034e924cb659
Antot <Anton_Chervatyuk@bk.ru>
2022-10-17 14:18:06 +0300

    Ёлочка

---
+++ Задача со звездой sem4/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите функцию, которая принимает одно число - 
+// высоту елочки и рисует ее в консоли звездочками.
+
+void Elka(int arg)
+{
+    for (int i = 0; i <= arg; i++)
+    {
+        for (int j = 0; j < arg - i; j++)
+        {
+            Console.Write(" ");
+        }
+        for (int z = 0; z < i * 2 - 1; z++)
+        {
+            Console.Write("*");
+        }
+        Console.Write("\n");
+    }
+}
+
+Console.Clear();
+Console.Write("Высота елки: ");
+int num = int.Parse(Console.ReadLine()!);
+
+Elka(num);
