dbbfc4eff08897badf06de7356690d292bdd88d5
Antot <Anton_Chervatyuk@bk.ru>
2022-10-17 14:13:05 +0300

    задача 29

---
+++ Задача 29/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив 
+// из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] New()
+{
+    Console.Write("Введите значения массива через запятую c пробелом: ");
+    string[] sign = Console.ReadLine()!.Split(", ");
+    int[] array;
+    if (sign.Length > 8)
+    {
+        array = new int[sign.Length];
+    }
+    else
+    {
+        array = new int[8];
+    }
+    for (int i = 0; i < sign.Length; i++)
+    {
+        array[i] = System.Int32.Parse(sign[i]);
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.WriteLine("Вывод заданного массива: ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}");
+        if (i != arr.Length - 1)
+        {
+            Console.Write(", ");
+
+        }
+    }
+
+}
+
+PrintArray(New());
